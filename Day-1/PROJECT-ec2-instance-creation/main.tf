8f5aa68762fa1f67bd9b9d158693e60cd36f909b
aquamo@gmail.com <103864675+ishaqahmad41455@users.noreply.github.com>
2024-08-28 13:08:29 +0500

    first commit

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,10 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws_instance" "test" {
+    ami           = "ami-0e86e20dae9224db8"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0929ac5c28728d8de"
+    key_name = "devops-keypair"
+
 }
