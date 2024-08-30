60b3009c74d6a3d8ef9d7e7442a73b61f5719d26
aquamo@gmail.com <103864675+ishaqahmad41455@users.noreply.github.com>
2024-08-30 15:01:22 +0500

    commit

--- Day-3/project.test/main.tf
+++ Day-3/project.test/main.tf
@@ -1,19 +1,3 @@
-variable "ami_value" {
-    description = "value for ami"
-  
-}
-
-variable "instance_type" {
-    description = "value for instance_type"
-  
-}
-
-variable "subnet_id" {
-    description = "value for subnet_id"
-  
-}
-
-
 provider "aws" {
     region = "us-east-1"
   
---
+++ Day-3/project.test/variable.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+    description = "value for ami"
+  
+}
+
+variable "instance_type" {
+    description = "value for instance_type"
+  
+}
+
+variable "subnet_id" {
+    description = "value for subnet_id"
+  
+}
